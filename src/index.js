3a3ef8afd394caea6aeb2190f45cce45cdb210d2
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-03-18 08:48:38 -0700

    more tuning of webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DragList from './list'
-
 // Put the things into the DOM!
 ReactDOM.render(<DragList />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,10 @@ module.exports = {
         }
       }
     ]
+  },
+	devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './html',
+    publicPath: '/dist'
   }
 };
