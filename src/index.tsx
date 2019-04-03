1d5d57edaabcc67a05f8cfac39bc4513f1a91ef0
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-04-02 20:29:10 -0700

    index tidy

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import App from './App'
 import LocationComponent from './LocationComponent'
 
-import App from './App'
 // Put the things into the DOM!
 ReactDOM.render(<App/>, document.getElementById('root'));
 // ReactDOM.render(<LocationComponent />, document.getElementById('root'));
