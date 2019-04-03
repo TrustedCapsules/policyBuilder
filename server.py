b166fc10e65ed6c8efdd70897f2544b42db4ab23
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-04-02 20:28:51 -0700

    upload fixes

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, send_from_directory, send_file
 from http import HTTPStatus
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
@@ -14,24 +16,26 @@ def home():
     return send_file('html/index.html')
 
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() == 'lua'
+
+
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
+            return 'No file part', HTTPStatus.BAD_REQUEST
         file = request.files['file']
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+            return 'No selected file', HTTPStatus.BAD_REQUEST
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+            return 'success', 200
 
 
 if __name__ == "__main__":
