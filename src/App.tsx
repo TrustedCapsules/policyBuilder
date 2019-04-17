baaf662e9db5c681946e93b9de8c315fa4a84a50
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-04-17 13:00:35 -0700

    add email fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import DragList from './DragList'
-import * as React from "react";
+import * as React from 'react';
 import {useDropzone} from 'react-dropzone'
 import * as superagent from "superagent";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
@@ -15,17 +15,38 @@ export default class App extends React.Component<{}, {}> {
 }
 
 function MyDropzone() {
+    const [email1, setEmail1] = React.useState("");
+    const [email2, setEmail2] = React.useState("");
+    const [inviteRecipients, setInviteRecipients] = React.useState(false);
     const onDrop = React.useCallback((acceptedFiles: File[]) => {
-        const req = superagent.post('http://127.0.0.1:5001/submit');
-        acceptedFiles.forEach((file: File) => {
-            console.log(file);
-            req.attach(file.name, file);
-        });
+        const req = superagent.post('http://127.0.0.1:5001/submit')
+            .field({email1})
+            .field({email2})
+            .field({inviteRecipients});
+        acceptedFiles.forEach((file: File) => req.attach(file.name, file));
         req.end();
-    }, []);
+    }, [email1, email2, inviteRecipients]);
     const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop});
 
-    return (
+    return (<>
+        <label> Email 1
+            <input type="email"
+                   name="email1"
+                   value={email1}
+                   onChange={e => setEmail1(e.target.value)}
+                   placeholder="Email 1"/>
+        </label>
+        <label> Email 2
+            <input type="email"
+                   name="email2"
+                   value={email2}
+                   onChange={e => setEmail2(e.target.value)}
+                   placeholder="Email 2"/>
+        </label>
+        <input type="checkbox"
+               name="inviteRecipients"
+               onChange={e => setInviteRecipients(e.target.checked)}
+               checked={inviteRecipients}/>
         <div {...getRootProps()}>
             <input {...getInputProps()} />
             {
@@ -40,5 +61,5 @@ function MyDropzone() {
                     </>
             }
         </div>
-    )
+    </>);
 }
